d5e6653e41121efb82a44c478a20efcb9a75943d
Mateusz Cogiel <jank7947@gmail.com>
2022-08-21 13:30:27 +0200

    Repositories
    Added repositories for Asnwer and Question Entities

---
+++ Forum.WebAPI/Repositories/AnswerRepository.cs
@@ -0,0 +1,75 @@
+﻿using Forum.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.WebAPI.Repositories;
+
+public interface IAnswerRepository : IDisposable
+{
+    IEnumerable<Answer> GetAnswer();
+    Answer GetAnswerByID(int answerId);
+    void InsertAnswer(Answer answer);
+    void DeleteAnswer(int answerId);
+    void UpdateAnswer(Answer answer);
+    void Save();
+}
+
+public class AnswerRepository : IAnswerRepository, IDisposable
+{
+    private DatabaseContext context;
+
+    public AnswerRepository(DatabaseContext context)
+    {
+        this.context = context;
+    }
+
+    public IEnumerable<Answer> GetAnswer()
+    {
+        return context.Answers.ToList();
+    }
+
+    public Answer GetAnswerByID(int id)
+    {
+        return context.Answers.Find(id);
+    }
+
+    public void InsertAnswer(Answer answer)
+    {
+        context.Answers.Add(answer);
+    }
+
+    public void DeleteAnswer(int answerID)
+    {
+        Answer answer = context.Answers.Find(answerID);
+        context.Answers.Remove(answer);
+    }
+
+    public void UpdateAnswer(Answer answer)
+    {
+        context.Entry(answer).State = EntityState.Modified;
+    }
+
+    public void Save()
+    {
+        context.SaveChanges();
+    }
+
+    private bool disposed = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!this.disposed)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+        }
+        this.disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
